2e5225d5151d65b47e596d3b23ae61f51f494546
Mary <32392607+MartFrida@users.noreply.github.com>
2024-02-11 22:27:26 +0100

    create controllers get/delete

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,45 @@
-import contactsService from "../services/contactsServices.js";
+import * as contactsService from "../services/contactsServices.js";
+import HttpError from '../helpers/HttpError.js'
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const result = await contactsService.listContacts()
+    res.status(200).json(result)
+  } catch (error) {
+    next(error)
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const result = await contactsService.getContactById(id)
+    if (!result) {
+      throw HttpError(404, "Not found")
+    }
+    res.status(200).json(result)
+  } catch (error) {
+    next(error)
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const result = await contactsService.removeContact(id)
 
-export const createContact = (req, res) => {};
+    if (!result) {
+      throw HttpError(404, "Not found")
+    }
+    res.status(200).json(result)
 
-export const updateContact = (req, res) => {};
+  } catch (error) {
+    next(error)
+  }
+};
+
+export const createContact = async (req, res, next) => {
+
+};
+
+export const updateContact = async (req, res, next) => { };
